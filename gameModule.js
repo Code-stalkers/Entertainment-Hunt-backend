44863e7a530e85a02fd1901a601ebe349b7c9ab1
Marwan-Zakia <marwanamir.ma@gmail.com>
2021-09-17 19:17:21 +0300

    new

--- gameModule.js
+++ gameModule.js
@@ -5,72 +5,44 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 const axios = require('axios');
-
+const PORT = 3001;
 
 //MongoDB
 // const mongoose = require('mongoose');
-const gameData = require('./gameModule');
 
 
-//  let gameData = async function (req, res) {
-//     const title = req.query.title;
-//     let url = await`https://www.omdbapi.com/?s=${title}&apikey=da2fe669&type=game`;
+// movie API key apikey=da2fe669
+// https://www.omdbapi.com/?s=SOMETHING&apikey=da2fe669 this is for serching for a movie title
+
+//https://www.omdbapi.com/?s=warzone&apikey=da2fe669&type=game
+class GAME {
+    constructor(Title,Poster,Type,Year) {
+        this.Title=Title
+        this.Poster=Poster
+        this.Type=Type
+        this.Year=Year
+        
+        
+    }
+}
+let gameData = async function (req, res) {
+    const title = req.query.title;
+    let url = await`https://www.omdbapi.com/?s=${title}&apikey=da2fe669&type=game`;
    
-//     await axios
-//         .get(url)
-//         .then(data => {
-//             let newGAME = data.data.Search.map(item => {
-//                 return new GAME(item.Title,item.Poster,item.Type,item.Year);
-//             })
-//             res.send(newGAME)
-//         })
-//         .catch(err => console.log(err))
-// }
+    await axios
+        .get(url)
+        .then(data => {
+            let newGAME = data.data.Search.map(item => {
+                return new GAME(item.Title,item.Poster,item.Type,item.Year);
+            })
+            res.send(newGAME)
+        })
+        .catch(err => console.log(err))
+}
 
-// class GAME {
-//     constructor(Title,Poster,Type,Year) {
-//         this.Title=Title
-//         this.Poster=Poster
-//         this.Type=Type
-//         this.Year=Year
-        
-        
-//     }
-// }
+
 server.get('/game', gameData);
-
-//// movie API key apikey=da2fe669
-//// https://www.omdbapi.com/?t=SOMETHING&apikey=da2fe669 this is for serching for a movie title
-
-// write an async function to use the api link like this
-// const movieData = async function (req, res) {
-//     let title = req.query.title;
-//     let url = await`https://www.omdbapi.com/?t=${title}&apikey=da2fe669`;
-
-//     await axios
-//         .get(url)
-//         .then(data => {
-
-//             let newMovie = data.map(item => {
-//                 return new Movie(item);
-//             })
-//           console.log(newMovie)
-//             res.send(newMovie)
-
-//         })
-//         .catch(err => console.log(err))
-// }
-
-// class Movie {
-//     constructor(apiitems) {
-//         this.items
-//     }
-// }
-
-//dont forget the route :
-// server.get('/movie', movieData);
-
-
+module.exports = gameData
 
 
 // main().catch(err => console.log(err));
