03a4a6a824f0743d094089ad09ba27db68e9a5d3
Mohammad-Haroun-97 <harounmohammad@yahoo.com>
2021-09-19 09:14:58 +0300

    push movie database section

--- Static.js
+++ Static.js
@@ -10,15 +10,66 @@ const axios = require('axios');
 // const PORT = 3001;
 
 
+///////////////////////mongo
+const mongoose = require('mongoose');
+
+let films; 
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(`mongodb://Mohammad-Haroun-97:awdsef135.@myfirstcluster-shard-00-00.xvzzr.mongodb.net:27017,myfirstcluster-shard-00-01.xvzzr.mongodb.net:27017,myfirstcluster-shard-00-02.xvzzr.mongodb.net:27017/Films?ssl=true&replicaSet=atlas-tlumk8-shard-0&authSource=admin&retryWrites=true&w=majority`);
+    const filmsSchema  = new mongoose.Schema({
+      Title: String,
+      Poster:String,
+      Type:String,
+      Year:String,
+     
+  });
+
+
+  films = mongoose.model('Films', filmsSchema);
+
+ saving()
 
 
 
+}
+
+async function saving() {
+
+
+
+
+
+    
+}
+
+
+function addingWatchlist(req,res) {
+    
+    const movieList=new films ({
+         Title: req.query.Title,
+       Poster : req.query.Poster,
+        Type : req.query.Type,
+        Year : req.query.Year})
+
+    // let movieList=req.query;
+    
+    console.log('final',req.query);
+
+    movieList.save();
+    res.send('hello')
+    
+}
+
+  ////////////////////////////////////
+
+// http://localhost:3001
 
 const staticMoviesFunction = async function (req,res) {
 
     
-
-    let url= await `https://www.omdbapi.com/?s=all&apikey=da2fe669&y=2021`;
+    let url= await `https://www.omdbapi.com/?s=war&apikey=da2fe669&y=2021`;
 
     await axios.get(url)
     
@@ -45,15 +96,13 @@ class Static{
 
         this.Title=Title;
         this.Poster=Poster;
-        // Type=item.plot;
+        
         this.Year=Year;
         this.Type=Type
-        
-
-
+       
     }
 
 }
 
 
-module.exports = staticMoviesFunction 
+module.exports ={ staticMoviesFunction , addingWatchlist }
--- server.js
+++ server.js
@@ -7,51 +7,52 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 const axios = require('axios');
-let PORT = process.env.PORT||3001;
+let PORT = 3001;
+
 
 //MongoDB
- const mongoose = require('mongoose');
- let gamesModel;
- main().catch(err => console.log(err));
+//  const mongoose = require('mongoose');
+//  let gamesModel;
+//  main().catch(err => console.log(err));
  
- async function main() {
-    await  mongoose.connect('mongodb://localhost:27017/game');
+//  async function main() {
+//     await  mongoose.connect('mongodb://localhost:27017/game');
  
-   const gameSchema = new mongoose.Schema({
-     Title: String,
-     Poster: String,
-     Type: String,
-     Year: String
-   });
+//    const gameSchema = new mongoose.Schema({
+//      Title: String,
+//      Poster: String,
+//      Type: String,
+//      Year: String
+//    });
  
-   gamesModel = mongoose.model('game', gameSchema);
+//    gamesModel = mongoose.model('game', gameSchema);
  
       // seedData();
- }
+//  }
  
  //seeding a data function 
- async function seedData() {
+//  async function seedData() {
     // const dataaa = require('./gameModule');
     // let Memory = dataaa.Memory;
     // Memory = new gamesModel
     // Memory.loadDatabase();
-   const game = new gamesModel({
-     Title: 'War and Peace',
-     Poster: `War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature: Pierre Bezukhov, the illegitimate son of a count who is fighting for his inheritance and yearning for spiritual fulfillment; Prince Andrei Bolkonsky, who leaves his family behind to fight in the war against Napoleon; and Natasha Rostov, the beautiful young daughter of a nobleman who intrigues both men.
-     A s Napoleon’s army invades, Tolstoy brilliantly follows characters from diverse backgrounds—peasants and nobility, civilians and soldiers—as they struggle with the problems unique to their era, their history, and their culture. And as the novel progresses, these characters transcend their specificity, becoming some of the most moving—and human—figures in world literature.
-     `,
-     Type: 'https://images-na.ssl-images-amazon.com/images/I/A1aDb5U5myL.jpg',
-     Year: 'marwanamir.ma@gmail.com'
-   });
+//    const game = new gamesModel({
+//      Title: 'War and Peace',
+//      Poster: `War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature: Pierre Bezukhov, the illegitimate son of a count who is fighting for his inheritance and yearning for spiritual fulfillment; Prince Andrei Bolkonsky, who leaves his family behind to fight in the war against Napoleon; and Natasha Rostov, the beautiful young daughter of a nobleman who intrigues both men.
+//      A s Napoleon’s army invades, Tolstoy brilliantly follows characters from diverse backgrounds—peasants and nobility, civilians and soldiers—as they struggle with the problems unique to their era, their history, and their culture. And as the novel progresses, these characters transcend their specificity, becoming some of the most moving—and human—figures in world literature.
+//      `,
+//      Type: 'https://images-na.ssl-images-amazon.com/images/I/A1aDb5U5myL.jpg',
+//      Year: 'marwanamir.ma@gmail.com'
+//    });
  
     
      
 
-     await game.save();
+    //  await game.save();
     //  await Memory.insert();
      
     //  console.log(Memory)
- }
+//  }
 
 
 // Routes
@@ -133,7 +134,9 @@ let PORT = process.env.PORT||3001;
 const gameData = require('./gameModule');
 
 
-const staticMoviesFunction=require('./Static')
+// const staticMoviesFunction=require('./Static')
+
+const {addingWatchlist,staticMoviesFunction}=require('./Static.js')
 const moviesFunction=require('./Movies')
 
 //MongoDB
@@ -147,6 +150,23 @@ server.get('/movie', moviesFunction);
 server.get('/static', staticMoviesFunction);
 
 // servers 
+
+// http://localhost:3001/addToWatchlist
+
+server.get('/addToWatchlist',addingWatchlist)
+
+
+// function addingWatchlist(req,res) {
+//     const movieList=new films ()
+
+//     let movieList=req.query;
+    
+//     console.log('final',req.query);
+
+//     movieList.save();
+//     res.send('hello')
+    
+// }
 
 // http://localhost:3001/game?title=dota
 
