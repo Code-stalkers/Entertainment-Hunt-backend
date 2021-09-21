bacb9f6d3ce4e185c117c88e3f77d2e470420231
ahmedbani <a13banisal@gmail.com>
2021-09-21 15:21:36 +0300

    all the changes modularized

--- Movies.js
+++ Movies.js
@@ -1,5 +1,5 @@
-
 'use strict'
+// const mongoose = require('mongoose');
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -8,12 +8,53 @@ server.use(cors());
 const axios = require('axios');
 const PORT = 3001;
 
+let obj ={};
+// let films; 
+// main().catch(err => console.log(err));
+
+
+// async function main() {
+//     await mongoose.connect(`mongodb://Mohammad-Haroun-97:awdsef135.@myfirstcluster-shard-00-00.xvzzr.mongodb.net:27017,myfirstcluster-shard-00-01.xvzzr.mongodb.net:27017,myfirstcluster-shard-00-02.xvzzr.mongodb.net:27017/Films?ssl=true&replicaSet=atlas-tlumk8-shard-0&authSource=admin&retryWrites=true&w=majority`);
+//     const filmsSchema  = new mongoose.Schema({
+//       Title: String,
+//       Poster:String,
+//       Type:String,
+//       Year:String,
+//       comment:String,
+     
+//   });
+
+
+  
+
+//  saving()
+
+
+
+// }
+
+// async function saving() {
+
+
+    
+// }
 
 
 
 
 
-const moviesFunction = async function (req,res) {
+
+
+
+
+
+
+
+
+// http:localhost:3001/static
+
+
+obj.moviesFunction = async function (req,res) {
 
     let title =req.query.title;
     let year =req.query.year;
@@ -28,7 +69,7 @@ const moviesFunction = async function (req,res) {
         
         let movieData=dataFromUrl.data.Search.map(item =>{
 
-            return new Movie(item.Title, item.Poster, item.Year,item.Type)
+            return new Movie(item.Title, item.Poster, item.Year,item.Type,item.comment)
          
     })
     console.log(movieData);
@@ -41,13 +82,14 @@ const moviesFunction = async function (req,res) {
 
 class Movie{
 
-    constructor(Title,Poster,Year,Type){
+    constructor(Title,Poster,Year,Type,comment){
 
         this.Title=Title;
         this.Poster=Poster;
         // Type=item.plot;
         this.Year=Year;
-        this.Type=Type
+        this.Type=Type;
+        this.comment='comment'
 
     }
 
@@ -55,14 +97,7 @@ class Movie{
 
 
 
-// http:localhost:3001/static
 
 
 
-
-
-
-
-
-
-module.exports = moviesFunction 
+module.exports = obj;
